ddc13e019de64553ba7e446c32406d2ef0c9f07d
humancatfood <janborchers09@googlemail.com>
2017-04-29 15:59:07 +0100

    Throttle API-hits

--- src/js/data/omdb-service.js
+++ src/js/data/omdb-service.js
@@ -1,17 +1,18 @@
 import { get } from 'axios';
 
+import throttle from 'lodash/throttle';
 
 
-export const getFilmList = searchTerm => {
+export const getFilmList = throttle(searchTerm => {
 
   return get(`http://www.omdbapi.com/?s=${ searchTerm }`)
     .then(response => response.data && response.data.Search || []);
 
-};
+}, 1000);
 
-export const getFilm = filmId => {
+export const getFilm = throttle(filmId => {
 
   return get(`http://www.omdbapi.com/?i=${ filmId }&plot=full`)
     .then(response => response.data.Error ? null : response.data);
 
-};
+}, 1000);
