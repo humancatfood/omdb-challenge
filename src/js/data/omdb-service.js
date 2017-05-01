001efde83a0688c558604e2d9899bb1d9dd95514
humancatfood <janborchers09@googlemail.com>
2017-05-01 02:54:35 +0100

    Use https for omdb

--- src/js/data/omdb-service.js
+++ src/js/data/omdb-service.js
@@ -4,14 +4,14 @@ import { get } from 'axios';
 
 export const getFilmList = searchTerm => {
 
-  return get(`http://www.omdbapi.com/?s=${ searchTerm }`)
+  return get(`https://www.omdbapi.com/?s=${ searchTerm }`)
     .then(response => response.data && response.data.Search || []);
 
 };
 
 export const getFilm = filmId => {
 
-  return get(`http://www.omdbapi.com/?i=${ filmId }&plot=full`)
+  return get(`https://www.omdbapi.com/?i=${ filmId }&plot=full`)
     .then(response => response.data.Error ? null : response.data);
 
 };
